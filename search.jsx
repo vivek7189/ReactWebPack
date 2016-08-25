05db5fada2d69055f6c9d0d3cec4909f3186ff6c
Vivek Kumar <vivek.kumar1@markitserv.com>
2016-08-25 17:48:19 +0530

    second

--- search.jsx
+++ search.jsx
@@ -2,10 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class SearchBar1 extends React.Component {
+    constructor() {
+		super();
+		// Initial state of the component
+        this.state = {filterText: 'fg'}
+    }
+    handleChange(e){
+        console.log('change',e.target.value,"value");
+        this.setState({
+            filterText:e.target.value
+        })
+    }
      render(){
         return (
             <div>fgfu
-                <input type="text" value="rt"/>
+                <input type="text" onChange= {this.handleChange.bind(this)} value={this.state.filterText} />
             </div>
         )
     }
@@ -13,3 +24,4 @@ class SearchBar1 extends React.Component {
 
 ReactDOM.render(<SearchBar1/>,document.getElementById('search'));
 
+ 
